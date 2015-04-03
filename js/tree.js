3b2cbd5dac533de7eee4f8801e69ff39920601ce
Agnès Haasser <ahaasser@tea-ebook.com>
2015-04-03 16:56:50 +0200

    Add mode strict and fix a bug

--- js/tree.js
+++ js/tree.js
@@ -1,4 +1,5 @@
 (function enableJsNavigation($) {
+    'use strict';
     var tree = {
         'web' : {
             'web' : 'http://blabla/web-web.html',
@@ -22,23 +23,28 @@
         $achat.append($option);
     }
 
-    $('#select_container').change('#select_achat', function updateLectureSelect(e) {
+    function updateLectureSelect(e) {
+        var $lecture;
         $lecture = $('<select></select>');
         $lecture.attr('id', 'select_lecture');
         achat = $achat.val();
-        for (lecture in tree[achat]) {
+        for (var lecture in tree[achat]) {
             $option = $('<option></option>');
             $option.val(lecture);
             $option.text(lecture);
             $lecture.append($option);
         }
         $('#select_lecture_wrapper').html($lecture);
-    });
+        $lecture.change();
+    }
+    $('#select_container').on('change', '#select_achat', updateLectureSelect);
 
-    $('#select_container').change('#select_lecture', function updateLectureSelect(e, t) {
+    function updateSelectResult(e, t) {
         var achat = $achat.val();
         var lecture = $(e.target).val();
+        console.log(achat, lecture);
         var resultat = tree[achat][lecture];
         $('#select_result').html(resultat);
-    });
+    }
+    $('#select_container').on('change', '#select_lecture', updateSelectResult);
 }(jQuery, undefined));
