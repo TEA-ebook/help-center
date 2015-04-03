e0393f7451b3e37bcc5fcd6009d0ca79f4e0f3aa
Agnès Haasser <ahaasser@tea-ebook.com>
2015-04-03 16:33:04 +0200

    Mod: no pollution of global namespace

--- js/tree.js
+++ js/tree.js
@@ -1,43 +1,44 @@
-var tree = {
-    'web' : {
-        'web' : 'http://blabla/web-web.html',
-        'ordinateur' : 'web-ordinateur',
-        'basic2' : 'web-liseuse'
-    },
-    'liseuse' : {
-        'cybook' : 'link liseuse-cybook',
-        'pbv4' : 'link liseuse-pb4'
-    },
-    'android' : {
-        'android' : 'link android'
+(function enableJsNavigation($) {
+    var tree = {
+        'web' : {
+            'web' : 'http://blabla/web-web.html',
+            'ordinateur' : 'web-ordinateur',
+            'basic2' : 'web-liseuse'
+        },
+        'liseuse' : {
+            'cybook' : 'link liseuse-cybook',
+            'pbv4' : 'link liseuse-pb4'
+        },
+        'android' : {
+            'android' : 'link android'
+        }
+    };
+
+    var $achat = $('#select_achat');
+    for (var achat in tree) {
+        var $option = $('<option></option>');
+        $option.val(achat);
+        $option.text(achat);
+        $achat.append($option);
     }
-};
 
-var $achat = $('#select_achat');
-for (var achat in tree) {
-    var $option = $('<option></option>');
-    $option.val(achat);
-    $option.text(achat);
-    $achat.append($option);
-}
+    $('#select_container').change('#select_achat', function updateLectureSelect(e) {
+        $lecture = $('<select></select>');
+        $lecture.attr('id', 'select_lecture');
+        achat = $achat.val();
+        for (lecture in tree[achat]) {
+            $option = $('<option></option>');
+            $option.val(lecture);
+            $option.text(lecture);
+            $lecture.append($option);
+        }
+        $('#select_lecture_wrapper').html($lecture);
+    });
 
-$('#select_container').change('#select_achat', function updateLectureSelect(e) {
-    $lecture = $('<select></select>');
-    $lecture.attr('id', 'select_lecture');
-    achat = $achat.val();
-    for (lecture in tree[achat]) {
-        $option = $('<option></option>');
-        $option.val(lecture);
-        $option.text(lecture);
-        $lecture.append($option);
-    }
-    $('#select_lecture_wrapper').html($lecture);
-});
-
-$('#select_container').change('#select_lecture', function updateLectureSelect(e, t) {
-    var achat = $achat.val();
-    var lecture = $(e.target).val();
-    var resultat = tree[achat][lecture];
-    $('#select_result').html(resultat);
-});
-
+    $('#select_container').change('#select_lecture', function updateLectureSelect(e, t) {
+        var achat = $achat.val();
+        var lecture = $(e.target).val();
+        var resultat = tree[achat][lecture];
+        $('#select_result').html(resultat);
+    });
+}(jQuery, undefined));
