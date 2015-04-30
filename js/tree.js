30be7e8946c09b4219e0da96f5503173e532cecf
Florie Séchaud <fsechaud@tea-ebook.com>
2015-04-30 16:24:45 +0200

    Add: wrapper for home navigation

--- js/tree.js
+++ js/tree.js
@@ -3,24 +3,24 @@
     var tree = {
         'le web' : {
             //'web' : '/web-web',
-            'un ordinateur' : '<a class="btn btn-default btn-home" role="button" href="/web-ordinateur">Acheter un ebook sur Internet et le lire sur son ordinateur</a>',
-            'une liseuse basic2' : '<a class="btn btn-default btn-home" role="button" href="/web-basic2">Acheter un ebook sur Internet et le lire sur sa liseuse Pocketbook Basic 2</a>',
-            'une liseuse cybook' : '<a class="btn btn-default btn-home" role="button" href="/web-cybook">Acheter un ebook sur Internet et le lire sur sa liseuse Cybook</a>',
-            'une liseuse Pocketbook firmware 4' : '<a class="btn btn-default btn-home" role="button" href="/web-pbv4">Acheter un ebook sur Internet et le lire sur sa liseuse Pocketbook v4</a>',
-            'une liseuse Pocketbook firmware 4 "MesAchats"' : '<a class="btn btn-default btn-home" role="button" href="/web-pbv4MesAchats">Acheter un ebook sur Internet et le lire sur sa liseuse Pocketbook v4 « mes achats »</a>',
-            'une liseuse Pocketbook firmware 5' : '<a class="btn btn-default btn-home" role="button" href="/web-pbv5">Acheter un ebook sur Internet et le lire sur sa liseuse Pocketbook v5</a>',
-            'l\'application android v1' : '<a class="btn btn-default btn-home" role="button" href="/web-androidv1">Acheter un ebook sur Internet et le lire sur sa tablette Android (app v1)</a>',
+            'un ordinateur' : ['/web-ordinateur','Acheter un ebook sur Internet et le lire sur son ordinateur</a>'],
+            'une liseuse basic2' : ['/web-basic2','Acheter un ebook sur Internet et le lire sur sa liseuse Pocketbook Basic 2</a>'],
+            'une liseuse cybook' : ['/web-cybook','Acheter un ebook sur Internet et le lire sur sa liseuse Cybook</a>'],
+            'une liseuse Pocketbook firmware 4' : ['/web-pbv4','Acheter un ebook sur Internet et le lire sur sa liseuse Pocketbook v4</a>'],
+            'une liseuse Pocketbook firmware 4 "MesAchats"' : ['/web-pbv4MesAchats','Acheter un ebook sur Internet et le lire sur sa liseuse Pocketbook v4 « mes achats »</a>'],
+            'une liseuse Pocketbook firmware 5' : ['/web-pbv5','Acheter un ebook sur Internet et le lire sur sa liseuse Pocketbook v5</a>'],
+            'l\'application android v1' : ['/web-androidv1','Acheter un ebook sur Internet et le lire sur sa tablette Android (app v1)</a>'],
             //'l\'application android v2' : '/web-androidv2',
-            'l\'application iOs v1' : '<a class="btn btn-default btn-home" role="button" href="/web-iosv1">Acheter un ebook sur Internet et le lire sur son iPad/iPhone (app v1)</a>'
+            'l\'application iOs v1' : ['/web-iosv1','Acheter un ebook sur Internet et le lire sur son iPad/iPhone (app v1)</a>']
             //'l\'application iOs v2' : '/web-iosv2'
         },
         'une liseuse' : {
-            'une liseuse cybook' : '<a class="btn btn-default btn-home" role="button" href="/liseuse-cybook">Acheter un ebook et le lire sur sa liseuse Cybook</a>',
-            'une liseuse Pocketbook firmware 4' : '<a class="btn btn-default btn-home" role="button" href="/liseuse-pbv4">Acheter un ebook et le lire sur sa liseuse Pocketbook v4</a>',
-            'une liseuse Pocketbook firmware 5' : '<a class="btn btn-default btn-home" role="button" href="/liseuse-pbv5">Acheter un ebook et le lire sur sa liseuse Pocketbook v5</a>'
+            'une liseuse cybook' : ['/liseuse-cybook','Acheter un ebook et le lire sur sa liseuse Cybook</a>'],
+            'une liseuse Pocketbook firmware 4' : ['/liseuse-pbv4','Acheter un ebook et le lire sur sa liseuse Pocketbook v4</a>'],
+            'une liseuse Pocketbook firmware 5' : ['/liseuse-pbv5','Acheter un ebook et le lire sur sa liseuse Pocketbook v5</a>']
         },
         'l\'application android v1' : {
-            'l\'application android v1' : '<a class="btn btn-default btn-home" role="button" href="/androidv1-androidv1">Acheter un ebook et le lire sur sa tablette Android (app v1)</a>'
+            'l\'application android v1' : ['/androidv1-androidv1','Acheter un ebook et le lire sur sa tablette Android (app v1)</a>']
         }
         /*'l\'application android v2' : {
             'l\'application android v2' : '/androidv2-androidv2'
@@ -57,7 +57,8 @@
         var lecture = $(e.target).val();
         console.log(achat, lecture);
         var resultat = tree[achat][lecture];
-        $('#select_result').html(resultat);
+        var htmlResultat = '<a class="btn btn-default btn-home" role="button" href="'+resultat[0]+'">'+resultat[1]+'</a>';
+        $('#select_result').html(htmlResultat);
     }
     $('#select_container').on('change', '#select_lecture', updateSelectResult);
 }(jQuery, undefined));
